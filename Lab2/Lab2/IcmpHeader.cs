3a3b7ab73074d66e0d5e1c7aee262d47d4b809e9
mJaJksJ <maksim.m00@mail.ru>
2021-11-02 13:09:43 +0300

    IcmpHeader без последнего поля

---
+++ Lab2/Lab2/IcmpHeader.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+
+namespace Lab2
+{
+    public class IcmpHeader
+    {
+        public byte Byte { get; set; }
+
+        public byte Code { get; set; }
+
+        public UInt16 ControlSum { get; set; }
+
+        public static int TypeSize => (8 + 8 + 16) / 8;
+
+        public byte[] Blob()
+        {
+            var blob = new List<byte>();
+            blob.Add(Byte);
+            blob.Add(Code);
+            blob.AddRange(BitConverter.GetBytes(ControlSum));
+            return blob.ToArray();
+        }
+
+        public static UInt16 SolveControlSum(UInt16[] buffer, int length)
+        {
+            UInt64 crc = 0;
+            // Вычисление CRC 
+            var ptr = 0;
+            while (length > 1)
+            {
+                crc += buffer[ptr++];
+                length -= sizeof(UInt16);
+            }
+
+            if (length != 0)
+            {
+                crc += (byte) buffer[ptr];
+            }
+
+            // Закончить вычисления 
+            crc = (crc >> 16) + (crc & 0xffff);
+            crc += (crc >> 16);
+
+            return (UInt16) (~crc);
+        }
+
+        public static byte[] SendIcmp(Socket s, IpHeader iph, IcmpHeader icmph, byte[] data)
+        {
+            int dataLength = 0;
+
+            // Вычисление длин пакета и заголовка.
+            byte headerLength = (byte) IcmpHeader.TypeSize;
+            UInt32 packetLength = (uint) ((int) headerLength + dataLength);
+            icmph.ControlSum = 0;
+
+            byte[] buffer = new byte[(int) packetLength];
+            for (int i = 0; i < (int) packetLength; i++)
+            {
+                buffer[i] = (byte) 0x00;
+            }
+
+            // Копирование заголовка пакета в буфер ( CRC равно 0).
+            for (int i = 0; i < headerLength; i++)
+            {
+                buffer[i] = (byte) icmph.Blob()[i];
+            }
+
+            // Вычисление CRC.
+            icmph.ControlSum = IcmpHeader.SolveControlSum(buffer.Select(_ => (UInt16) _).ToArray(), (int) packetLength);
+
+            // Копирование заголовка пакета в буфер (CRC посчитана).
+            for (int i = 0; i < headerLength; i++)
+            {
+                buffer[i] = (byte) icmph.Blob()[i];
+            }
+
+            return buffer;
+        }
+    }
+}
---
+++ Lab2/Lab2/IpHeader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab2
+{
+    public class IpHeader
+    {
+        byte VerIhl { get; set; } // Длина заголовка (4 бита)  (измеряется в словах по 32 бита) + Номер версии протокола (4 бита)
+        byte Tos { get; set; }  // Тип сервиса 
+        UInt16 Tlen { get; set; } // Общая длина пакета 
+        UInt16 Id { get; set; } // Идентификатор пакета
+        UInt16 FlagsFo { get; set; } // Управляющие флаги (3 бита) + Смещение фрагмента (13 бит)
+        byte Ttl { get; set; } // Время жизни пакета
+        byte Proto { get; set; } // Протокол верхнего уровня 
+        UInt16 Crc { get; set; } // CRC заголовка
+        UInt32 SrcAddr { get; set; } // IP-адрес отправителя
+        UInt32 DstAddr { get; set; } // IP-адрес получателя
+
+    }
+}
