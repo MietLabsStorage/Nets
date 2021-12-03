32e0a5a50063aa95dedd7a6e48a5f29a37494367
mJaJksJ <maksim.m00@mail.ru>
2021-12-03 15:45:02 +0300

    smtp-client

---
+++ Lab4/Smtp/Smtp/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace Smtp
+{
+    partial class SmtpClient
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btSend = new System.Windows.Forms.Button();
+            this.tbTo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbFrom = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbSubject = new System.Windows.Forms.TextBox();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.btAttach = new System.Windows.Forms.Button();
+            this.lbAttach = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btSend
+            // 
+            this.btSend.Location = new System.Drawing.Point(12, 289);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(148, 25);
+            this.btSend.TabIndex = 0;
+            this.btSend.Text = "Send";
+            this.btSend.UseVisualStyleBackColor = true;
+            this.btSend.Click += new System.EventHandler(this.btSend_Click);
+            // 
+            // tbTo
+            // 
+            this.tbTo.Location = new System.Drawing.Point(61, 12);
+            this.tbTo.Name = "tbTo";
+            this.tbTo.Size = new System.Drawing.Size(100, 23);
+            this.tbTo.TabIndex = 1;
+            this.tbTo.Text = "porfirev2003@bk.ru";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "To";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "From";
+            // 
+            // tbFrom
+            // 
+            this.tbFrom.Location = new System.Drawing.Point(60, 41);
+            this.tbFrom.Name = "tbFrom";
+            this.tbFrom.Size = new System.Drawing.Size(100, 23);
+            this.tbFrom.TabIndex = 3;
+            this.tbFrom.Text = "mjajksj.test@mail.ru";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Subject";
+            // 
+            // tbSubject
+            // 
+            this.tbSubject.Location = new System.Drawing.Point(60, 70);
+            this.tbSubject.Name = "tbSubject";
+            this.tbSubject.Size = new System.Drawing.Size(100, 23);
+            this.tbSubject.TabIndex = 5;
+            this.tbSubject.Text = "Тестовое ообщение";
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(12, 99);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(148, 130);
+            this.tbMessage.TabIndex = 7;
+            this.tbMessage.Text = "Это тестовое сообщения разработки smtp";
+            // 
+            // btAttach
+            // 
+            this.btAttach.Location = new System.Drawing.Point(12, 235);
+            this.btAttach.Name = "btAttach";
+            this.btAttach.Size = new System.Drawing.Size(149, 23);
+            this.btAttach.TabIndex = 8;
+            this.btAttach.Text = "Attachment";
+            this.btAttach.UseVisualStyleBackColor = true;
+            this.btAttach.Click += new System.EventHandler(this.btAttach_Click);
+            // 
+            // lbAttach
+            // 
+            this.lbAttach.AutoSize = true;
+            this.lbAttach.Location = new System.Drawing.Point(12, 261);
+            this.lbAttach.Name = "lbAttach";
+            this.lbAttach.Size = new System.Drawing.Size(59, 15);
+            this.lbAttach.TabIndex = 9;
+            this.lbAttach.Text = "No attach";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // SmtpClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(172, 328);
+            this.Controls.Add(this.lbAttach);
+            this.Controls.Add(this.btAttach);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbSubject);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbFrom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbTo);
+            this.Controls.Add(this.btSend);
+            this.Name = "SmtpClient";
+            this.Text = "SmtpClient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btSend;
+        private System.Windows.Forms.TextBox tbTo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbFrom;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbSubject;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.Button btAttach;
+        private System.Windows.Forms.Label lbAttach;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ Lab4/Smtp/Smtp/Form1.cs
@@ -0,0 +1,72 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Smtp
+{
+    public partial class SmtpClient : Form
+    {
+        public SmtpClient()
+        {
+            InitializeComponent();
+        }
+
+        string attachPath = "";
+
+		private void btSend_Click(object sender, EventArgs e)
+		{
+			var message = new MimeMessage();
+			message.From.Add(new MailboxAddress(tbFrom.Text));
+			message.To.Add(new MailboxAddress(tbTo.Text));
+			message.Subject = tbSubject.Text;
+
+            var builder = new BodyBuilder();
+            builder.TextBody = tbMessage.Text;
+            if (!string.IsNullOrEmpty(attachPath))
+            { 
+                builder.Attachments.Add(attachPath); 
+            }
+
+            message.Body = builder.ToMessageBody();
+
+            using (var client = new MailKit.Net.Smtp.SmtpClient())
+            {
+                client.Connect("smtp.mail.ru", 465, true);
+
+                client.Authenticate("mjajksj.test@mail.ru", "password");
+
+                client.Send(message);
+                client.Disconnect(true);
+                MessageBox.Show("Sended");
+            }
+		}
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btAttach_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.InitialDirectory = "D:";
+            dialog.Title = "Выбор архива";
+            var result = dialog.ShowDialog();
+            lbAttach.Text = dialog.SafeFileName;
+            attachPath = dialog.FileName;
+        }
+    }
+}
